8680cbd72b2f9ac68b6e7431e7f10114e5a8e2a5
asgodev <asgodev@gmail.com>
2026-06-21 17:10:41 -0400

    fix: solucionado problema de colores en Logo

--- frontend/src/components/ui/Logo.jsx
+++ frontend/src/components/ui/Logo.jsx
@@ -1,11 +1,12 @@
 export const Logo = ({ inverted = false, size = 'text-5xl' }) => {
-    const colorA = inverted ? 'primary' : '[#fff]'
-    const colorB = inverted ? '[#fff]' : 'primary'
-    const colorC = '[#FFC907]'
     return (
         <h1 className={`${size} text-center`}>
-            <span className={`font-['Orbitron'] text-${colorA} font-black`}>comun</span>
-            <span className={`font-['Bungee'] bg-${colorA} text-${colorB} px-1 rounded-lg`}>APP</span>
+            <span className={`font-['Orbitron'] font-black ${inverted ? 'text-primary' : 'text-white'}`}>
+                comun
+            </span>
+            <span className={`font-['Bungee'] px-1 rounded-lg ${inverted ? 'bg-primary text-white' : 'bg-white text-primary'}`}>
+                APP
+            </span>
         </h1>
     )
 }
